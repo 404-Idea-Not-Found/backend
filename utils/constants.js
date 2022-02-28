a44a080c7e8bcdb101f6834a7b9e7def694c2e98
DanK <ddad9583@gmail.com>
2022-02-28 17:44:27 +0900

    [2] add error statement constant.

--- utils/constants.js
+++ utils/constants.js
@@ -4,6 +4,7 @@ exports.ERROR_MESSAGES = {
   FAILED_TO_GET_404_TOKEN: "ERROR: Failed to get 404 token from request",
   FAILED_TO_GET_USER: "ERROR: Failed to get user from DB",
   FAILED_TO_COMMUNICATE_WITH_DB: "ERROR: Failed to communicate with DB",
+  FAILED_TO_VERIFY_TOKEN: "ERROR: Failed to verify 404 token.",
 };
 exports.RESPONSE_RESULT = {
   ERROR: "error",
